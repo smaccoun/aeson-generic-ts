ceceab621bd81c995d134e2afeb9a0ded6342647
Steven MacCoun <theraccoun@gmail.com>
2018-09-15 18:00:31 -0400

    Styling with brittany

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,16 @@
 
 module Main where
 
-import GenericMappings
-import GHC.Generics
-import Data.Text
-import Typescript.Generate
-import Data.Maybe
+import           Data.Maybe
+import           Data.Text
+import           GenericMappings
+import           GHC.Generics
+import           Typescript.Generate
 
 data User =
   User
-    {name         :: Text
-    ,age          :: Int
+    {name :: Text
+    ,age  :: Int
     } deriving (Generic, TypescriptType)
 
 sampleUser :: User
@@ -19,5 +19,8 @@ sampleUser = User "Jane Smith" 45
 
 main :: IO ()
 main = do
-  putStrLn $ fromMaybe "" $ (unpack . toTypescript) <$> toTypescriptType sampleUser
+  putStrLn
+    $   fromMaybe ""
+    $   (unpack . toTypescript)
+    <$> toTypescriptType sampleUser
   putStrLn $ show $ toTypescriptType sampleUser
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,15 +2,15 @@
 
 module Spec where
 
-import Test.Hspec
-import GenericMappings
-import Data.Text
-import Typescript.Types
-import GHC.Generics
+import           Data.Text
+import           GenericMappings
+import           GHC.Generics
+import           Test.Hspec
+import           Typescript.Types
 
 data ARecord =
   ARecord
-    {firstField :: Int
+    {firstField   :: Int
     ,aStringField :: Text
     } deriving (Generic, TypescriptType)
 
@@ -18,9 +18,15 @@ sample :: ARecord
 sample = ARecord 3 "Meow"
 
 aesonGenericTSSpec :: Spec
-aesonGenericTSSpec =  do
+aesonGenericTSSpec = do
   describe "Record to interface " $ do
-    it "Should match the given TStype" $
-      toTypescriptType sample `shouldBe`
-        Just (TSInterface "ARecord" [TSField (FieldName "firstField") (TSPrimitiveType TSNumber),TSField (FieldName "aStringField") (TSPrimitiveType TSString)])
+    it "Should match the given TStype"
+      $          toTypescriptType sample
+      `shouldBe` (Just $ TSInterface
+                   "ARecord"
+                   [ TSField (FieldName "firstField") (TSPrimitiveType TSNumber)
+                   , TSField (FieldName "aStringField")
+                             (TSPrimitiveType TSString)
+                   ]
+                 )
 
