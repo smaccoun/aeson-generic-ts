6a80b2d4073a7c2c99f651d3e95ea397e7c4b836
Steven MacCoun <theraccoun@gmail.com>
2018-09-20 22:25:22 -0400

    Expose lib dir

--- src/Main.hs
+++ src/Main.hs
@@ -9,17 +9,17 @@ import           GHC.Generics
 import           Typescript.Generate
 
 data User = User
-    {name         :: PersonName
-    ,age          :: Int
-    ,placesLived  :: [Text]
-    ,isRegistered :: Bool
+    { name         :: PersonName
+    , age          :: Int
+    , placesLived  :: [Text]
+    , isRegistered :: Bool
     } deriving (Generic, TypescriptType)
 
 data PersonName =
   PersonName
-   {firstName     :: Text
-   ,middleInitial :: Maybe Text
-   ,lastName      :: Text
+   { firstName     :: Text
+   , middleInitial :: Maybe Text
+   , lastName      :: Text
    } deriving (Generic, TypescriptType)
 
 main :: IO ()
