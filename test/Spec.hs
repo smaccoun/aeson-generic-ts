94fbf15461a841aae5d21b04d7b16791ada304a5
Steven MacCoun <theraccoun@gmail.com>
2018-10-07 15:53:07 -0400

    Fixed tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,17 +2,19 @@
 
 module Spec where
 
+import           Bridge.Generics
+import           BridgeToTypescript
 import           Data.Text
 import           GenericMappings
 import           GHC.Generics
-import           Internal.Types
 import           Test.Hspec
+import           Typescript.Types
 
 data ARecord =
   ARecord
     {firstField   :: Int
     ,aStringField :: Text
-    } deriving (Generic, TypescriptType)
+    } deriving (Generic, TypescriptType, BridgeType)
 
 sample :: ARecord
 sample = ARecord 3 "Meow"
@@ -22,6 +24,15 @@ aesonGenericTSSpec = do
   describe "Record to interface " $ do
     it "Should match the given TStype"
       $          toTypescriptType sample
+      `shouldBe` (Just $ TSInterface
+                   "ARecord"
+                   [ TSField (FieldName "firstField") (TSPrimitiveType TSNumber)
+                   , TSField (FieldName "aStringField")
+                             (TSPrimitiveType TSString)
+                   ]
+                 )
+    it "Should match the given TStype from Bridge" $
+      (bridgeTypeToTSType <$> toBridgeType sample)
       `shouldBe` (Just $ TSInterface
                    "ARecord"
                    [ TSField (FieldName "firstField") (TSPrimitiveType TSNumber)
