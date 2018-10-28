3f0d65bf95b87d14d4870c1370449dc83260eb1d
Steven MacCoun <theraccoun@gmail.com>
2018-10-28 15:39:03 -0400

    Started adding some haddocks comments

--- src/Internal/Output/Foreign.hs
+++ src/Internal/Output/Foreign.hs
@@ -5,9 +5,20 @@ module Internal.Output.Foreign where
 import Data.Text
 import GHC.Generics
 
+{-|
+  Instantiate this class for all types that will be printed out as Text.
+-}
 class IsForeignType t where
   toForeignType :: t -> (ForeignType t)
 
+{-|
+  A type that represents a reference and a declaration.
+  The reference is used in data structures referring to this type,
+  while the declaration represent the full declaration of the type. For example, here the data type R makes reference to another data type S. The second line is the declaration for S, and the type ref in R is the refName.
+
+> data R = R {sField :: S}
+> data S = S {a      :: Int}
+-}
 data ForeignType t =
   ForeignType
     {refName     :: Text
