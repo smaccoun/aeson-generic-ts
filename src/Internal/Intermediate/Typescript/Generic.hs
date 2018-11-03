2e58c48ade001ab4282249330e256640d7b2eb17
Steven MacCoun <theraccoun@gmail.com>
2018-11-03 12:06:32 -0400

    Generic instances for primitives

--- src/Internal/Intermediate/Typescript/Generic.hs
+++ src/Internal/Intermediate/Typescript/Generic.hs
@@ -5,15 +5,20 @@ module Internal.Intermediate.Typescript.Generic where
 import GHC.Generics
 import Internal.Intermediate.Typescript.Lang
 import Data.Text
-import Data.Proxy
 
 class Typescript a where
-  toTSType :: a -> TSType flavor
-  default toTSType :: (Generic a, GenericTSType (Rep a)) => a -> TSType flavor
-  toTSType = genericToTS . from
+  toTSIntermediate :: a -> TSIntermediate flavor
+  default toTSIntermediate :: (Generic a, GenericTSIntermediate (Rep a)) => a -> TSIntermediate flavor
+  toTSIntermediate = genericToTS . from
 
-class GenericTSType hkf where
-  genericToTS :: hkf a -> TSType flavor
+class GenericTSIntermediate hkf where
+  genericToTS :: hkf a -> TSIntermediate flavor
 
 instance Typescript Text where
-  toTSType _ = TSPrimitiveType TSString
+  toTSIntermediate _ = TSPrimitiveType TSString
+
+instance (Num a) => Typescript a where
+  toTSIntermediate _ = TSPrimitiveType TSNumber
+
+instance Typescript Bool where
+  toTSIntermediate _ = TSPrimitiveType TSBoolean
