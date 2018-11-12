0aea4f622a4de4a979576c867d04afeb5aae0414
Steven MacCoun <theraccoun@gmail.com>
2018-11-11 20:45:04 -0500

    Switch to hspec discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,2 @@
-module Spec where
-
-import           FpTsSpec
-import           Test.Hspec
-import           VanillaSpec
-
-
-aesonGenericTSSpec :: Spec
-aesonGenericTSSpec = do
-  FpTsSpec.spec
-  VanillaSpec.spec
-
+-- file test/Spec.hs
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
