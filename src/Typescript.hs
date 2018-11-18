5329749343a3e93de7fe41a2f9e30e7141e1eadc
Steven MacCoun <theraccoun@gmail.com>
2018-11-18 17:47:09 -0500

    Export intermediate lang

--- src/Typescript.hs
+++ src/Typescript.hs
@@ -1,11 +1,12 @@
 module Typescript
-  (module Internal.Typescript.Flavors.Vanilla
+  (module Internal.Intermediate.Typescript.Lang
+  ,module Internal.Typescript.Flavors.Vanilla
   ,module Internal.Typescript.Flavors.FpTs
   ,TypescriptType
   )
 where
 
 import           Internal.Intermediate.Typescript.Generic (TypescriptType)
+import           Internal.Intermediate.Typescript.Lang
 import           Internal.Typescript.Flavors.FpTs
 import           Internal.Typescript.Flavors.Vanilla
-
