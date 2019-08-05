d6eea3f5adf2498681dc8d8d62dec3d2ee2e5291
Steven MacCoun <theraccoun@gmail.com>
2019-08-05 12:02:27 -0400

    Started adding template haskell derivations

---
+++ src/Typescript/TH/GenInstances.hs
@@ -0,0 +1,18 @@
+-- | Quick helper functions for (recursively) generating typescript instances using template haskell
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Typescript.TH.GenInstances where
+
+import Language.Haskell.TH.ReifyMany
+import Typescript.Internal.Intermediate.Generic (TypescriptType)
+import Language.Haskell.TH
+
+deriveTypescriptTypesRecursively :: [Name] -> Q [Dec]
+deriveTypescriptTypesRecursively nms = do
+  names <- reifyManyWithoutInstances ''TypescriptType nms (const True)
+  let allInstances = deriveTSInstance <$> names
+  pure allInstances
+
+deriveTSInstance :: Name -> Dec
+deriveTSInstance nm = StandaloneDerivD Nothing [] (ConT nm)
